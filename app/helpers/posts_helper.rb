d3cff7ffe3d0fc426a2e245a34afcba2383eb329
Brendan Berman <brendan.berman@gmail.com>
2015-05-21 12:55:48 -0700

    add likes counter and button to post views

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,14 @@
 module PostsHelper
+	def likes_id(post)
+		'likes-count-' + post.id.to_s
+	end
+
+	def like_button_text(post)
+		# Make sure the button reads correctly on load
+		if post.likes.pluck(:user_id).include? @user.id 
+			'Dislike' 
+		else 
+			'Like' 
+		end
+	end
 end
